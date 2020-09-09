bbe767da71d8631466715ed25eb8605826e78b60
Babu Aman Singh <65435598+babuaman19@users.noreply.github.com>
2020-09-09 17:23:42 +0530

    solving bugs-2

--- src/lab/exp3/simulation/advControlFlow.js
+++ src/lab/exp3/simulation/advControlFlow.js
@@ -34,7 +34,20 @@ window.view = {
  	},
 	getInput: function() {
 		var inputValue = document.getElementById('simpleLoopInput').value
-		model.inp = Number(inputValue)
+	
+		if (inputValue < '0' || inputValue >20)
+		{
+			alert("Invalid input")
+		}
+		else if (inputValue < '0' || inputValue >20)
+		{
+			alert("Invalid input")
+		}
+		else if (inputValue < '0' || inputValue >20)
+		{
+		 	alert("Invalid input")
+		}
+		else (model.inp = Number(inputValue))
 		this.clearExecutionSection()
 	},
 	getNestedInput: function() {
@@ -167,6 +180,19 @@ window.view = {
     	this.clearDivs()
     	var selected_loop = this.getSelectedLoop()
 		var inputValue = document.getElementById('simpleLoopInput').value
+		
+		if (selected_loop === 'for' && inputValue < '0' && inputValue >20 )
+		{
+			alert("Invalid input")
+		}
+		if (selected_loop === 'while' && inputValue < '0' && inputValue >20)
+		{
+			alert("Invalid input")
+		}
+		if (selected_loop === 'do-while' && inputValue < '0' && inputValue >20)
+		{
+		 	alert("Invalid input")
+		}
 		if (selected_loop === 'for' && inputValue !== '' && !isNaN(model.inp) )
 		{
 			this.displayLoop('forLoopContent', 'codeContentFor1')
@@ -179,6 +205,7 @@ window.view = {
 		{
 		 	this.displayLoop('dowhileLoopContent', 'codeContentDoWhile1')
 		}
+	    
 		this.disableButton('btnStart')
 		this.changeClass( 'btnStart', 'buttonDisable startButton')
 		this.enableButton('btnNext')
